50a34825f18ba20ce0af3a57833cd8cee439c4b7
DenisVolhonsky <dvolhonsky@gmail.com>
2018-12-27 12:58:17 +0200

    boilerplate created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from './Logo/Logo';
+
+const Header = () => (
+  <div>
+    <Logo></Logo>
+  </div>
+)
+
+
+export default Header
---
+++ src/components/Header/Logo/Logo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import logo from '../../../assets/img/logo.jpg' 
+
+const Logo = () => (
+  <img src={logo} alt="logo" width="70px" height="70px" />
+)
+
+export default Logo
